7bc23fc368c3e77327a7037d9eb03264d96884f3
Viviane <oviviane772@gmail.com>
2021-05-31 13:37:34 -0300

    Home

---
+++ mylink/src/pages/MyLinks/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import StatusBarPage from '../../components/StatusBarPage'
+
+export default function MyLinks(){
+  return(
+    <View>
+      <StatusBarPage
+      barStyle = "light-content"
+      backgroundColor="#132742"
+    />
+      <Text>
+        Pagina Links
+      </Text>
+    </View>
+  )
+}
